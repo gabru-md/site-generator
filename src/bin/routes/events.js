d4ecab5326c86d789a86e28840bd92c8331003f1
Manish Devgan <manish.nsit8@gmail.com>
2018-12-29 23:28:52 +0530

    Adds Add-Event
    Adds Add-Event

--- src/bin/routes/events.js
+++ src/bin/routes/events.js
@@ -4,10 +4,7 @@ const fs = require('fs');
 const _dataPath = path.join(__dirname,'../../data/events.json');
 var router = express.Router();
 
-const _data = JSON.parse(
-  fs.readFileSync(_dataPath));
-
-const _events = _data.events;
+var _data, _events;
 
 const searchEvent = (eventname) => new Promise( (resolve, reject) => {
   for (var i = _events.length - 1; i >= 0; i--) {
@@ -16,9 +13,42 @@ const searchEvent = (eventname) => new Promise( (resolve, reject) => {
     }
   }
   reject('Event not found');
-})
+});
+
+const createEvent = (_data) => new Promise( (resolve, reject) => {
+  var event = {};
+  for (key in _data) {
+    if( _data[key].constructor === Array) {
+      continue;
+    }
+
+    if( ((_data[key].constructor === String) || (__data[key].constructor === Object))
+        && (! _data[key])) {
+      reject('Not enough Data');
+    }
+
+    event[key] = _data[key];
+  }
+
+  resolve(event);
+});
+
+const saveEvent = (_data, event) => new Promise( (resolve, reject) => {
+  _data.events.push(event);
+  fs.writeFile(_dataPath, JSON.stringify(_data, null, 4), (err) => {
+    if(err) {
+      console.log(err);
+      reject(err);
+    }
+    resolve("File Saved");
+  });
+});
+
 
 router.get('/', (req, res, next) => {
+  _data = JSON.parse(
+  fs.readFileSync(_dataPath));
+  _events = _data.events;
   res.render('events',
     {
       events : _events,
@@ -27,9 +57,46 @@ router.get('/', (req, res, next) => {
       title : 'Events | Moksha'
     }
   );
-})
+});
+
+router.get('/add_event', (req, res, next) => {
+  res.render('add_event',
+      {
+        style : 'add_event.css',
+        js : 'add_event.js',
+        title : 'Add Event | Moksha'
+      }
+    );
+});
+
+
+router.post('/add_event', (req, res, next) => {
+  _data = JSON.parse(
+    fs.readFileSync(_dataPath));
+
+  createEvent(req.body)
+    .then((event) => {
+      saveEvent(_data, event)
+        .then((msg) => {
+          console.log(msg);
+          res.send('Done');
+        })
+        .catch((err) => {
+          console.log(err);
+          res.send('Cannot save! Please check the console');
+        })
+    })
+    .catch((err) => {
+      console.log(err);
+      res.send('Incomplete Data');
+    });
+});
+
 
 router.get('/:eventname', (req, res, next) => {
+  _data = JSON.parse(
+    fs.readFileSync(_dataPath));
+  _events = _data.events;
   const eventname = req.params.eventname;
   searchEvent(eventname)
     .then((event) => {
@@ -47,6 +114,9 @@ router.get('/:eventname', (req, res, next) => {
       console.log(err);
       res.render("404");
     });
-})
+});
+
+
+
 
 module.exports = router;
